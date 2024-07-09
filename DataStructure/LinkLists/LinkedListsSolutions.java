7a7cc10e2df9579c2a0de4a6b3d052b8a6746a31
TamakoP <643327987@qq.com>
2024-07-09 23:34:05 +0800

    alter

--- DataStructure/LinkLists/LinkedListsSolutions.java
+++ DataStructure/LinkLists/LinkedListsSolutions.java
@@ -12,24 +12,18 @@ public class LinkedListsSolutions {
 
     /**
      * @date 2024/7/9 下午5:11
-     * @Description 等等 还没思索完
+     * @Description 寻找两个链表的交叉节点
      * @param headA headB
      * @return LinkLists.ListNode
      */
     public ListNode getIntersectionNode(ListNode headA,ListNode headB){
         ListNode l1=headA,l2=headB;
 
-        if (l1.next != null) {
-            l1 = l1.next;
+        while(l1!=l2){
+            l1=(l1==null)?headB:l1.next;
+            l2=(l2==null)?headA:l2.next;
         }
-        if (l2.next != null) {
-            l2 = l2.next;
-        }
-        //此时l1和l2都指向链表的最后一个元素
-        headA = l1;
-        headB = l2;
         return l1;
-
 
     }
 }
