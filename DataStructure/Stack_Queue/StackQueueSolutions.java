8cb5ce0fc34473885252634ac4b71808e90c9f71
TamakoP <643327987@qq.com>
2024-07-17 22:43:56 +0800

    新增一个实现

--- DataStructure/Stack_Queue/StackQueueSolutions.java
+++ DataStructure/Stack_Queue/StackQueueSolutions.java
@@ -34,4 +34,26 @@ public class StackQueueSolutions {
         //若全部匹配成功，则栈为空，否则栈中还有元素
         return stack.isEmpty();
     }
+    /**
+     * @Description 数组中元素与下一个比它大的元素之间的距离:将数组中的元素依次入栈，将下一个压栈的元素宇当前栈顶元素相比较，若>，则距离为1
+     * @Param [temperatures]
+     * @return int[]
+     */
+    /*Input: [73, 74, 75, 71, 69, 72, 76, 73]
+      Output: [1, 1, 4, 2, 1, 1, 0, 0]
+     */
+    public int[] dailyTemperatures(int[] temperatures){
+        //stack存放对应元素的距离其最近并且大于其元素的下标
+        Stack<Integer> stack=new Stack<>();
+        int[] distance=new int[temperatures.length];
+        for(int i=0;i<temperatures.length;i++){
+            while(!stack.isEmpty()&&temperatures[i]>temperatures[stack.peek()]){
+                  int j=stack.pop();
+                  distance[j]=i-j;
+            }
+            //数组首元素的stack值设置为0，压栈操作放在比较之后能确保比较时stack的栈顶是上一个元素的下标
+            stack.push(i);
+        }
+        return distance;
+    }
 }
